ba5142fa325ee9e9a13e035dca7f0dc0df339648
Jorge Lewis <antopiahk@gmail.com>
2023-01-08 00:04:09 +0800

    small ui changes

--- src/components/molecules/big_text_field/mod.rs
+++ src/components/molecules/big_text_field/mod.rs
@@ -1,4 +1,3 @@
-
 use yew::{function_component, html, Html};
 
 mod props;
@@ -8,14 +7,13 @@ use crate::components::*;
 
 #[function_component]
 pub fn BigTextField(props: &Props) -> Html {
-    let Props { label, title, place_holder, required } = props;
-    html! {
-        <div>
-                                <label for={label.to_owned()}
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{title}</label>
-                                <input type="text" id={label.to_owned()}
-                                    class="bg-secondary-200 border border-secondary-300 text-secondary-900 text-sm text- rounded-lg focus:ring-accent-500 focus:border-accent-500 block w-full p-2 pb-16"
-                                    placeholder={place_holder.to_owned()} required={required.to_owned()} />
-                            </div>
-    }
+let Props { label, title, place_holder, required } = props;
+html! {
+<div>
+    <label for={label.to_owned()} class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{title}</label>
+    <input type="text" id={label.to_owned()}
+        class="bg-secondary-100 border border-secondary-300 text-secondary-900 text-sm text- rounded-lg focus:ring-accent-500 focus:border-accent-500 block w-full p-2 pb-16"
+        placeholder={place_holder.to_owned()} required={required.to_owned()} />
+</div>
+}
 }
--- src/components/molecules/text_field/mod.rs
+++ src/components/molecules/text_field/mod.rs
@@ -14,7 +14,7 @@ pub fn TextField(props: &Props) -> Html {
                                 <label for={label.to_owned()}
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{title}</label>
                                 <input type="text" id={label.to_owned()}
-                                    class="bg-secondary-200 border border-secondary-300 text-secondary-900 text-sm rounded-lg focus:ring-accent-500 focus:border-accent-500 block w-full p-2.5"
+                                    class="bg-secondary-100 border border-secondary-300 text-secondary-900 text-sm rounded-lg focus:ring-accent-500 focus:border-accent-500 block w-full p-2.5"
                                     placeholder={place_holder.to_owned()} required={required.to_owned()} />
                             </div>
     }
