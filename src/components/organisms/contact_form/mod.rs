7b5cfdbbe97e150a72825281957253bd8e4b6d62
Jorge Lewis <antopiahk@gmail.com>
2023-01-07 20:46:58 +0800

    work on contact form and big text field

--- src/components/organisms/contact_form/mod.rs
+++ src/components/organisms/contact_form/mod.rs
@@ -10,12 +10,13 @@ pub fn ContactForm(props: &Props) -> Html {
     html! {
         <form>
                         <div class="grid gap-6 mb-6 md:grid-cols-2">
-                            <TextField label="first_name" title="Jorge" place_holder="Lewis" required=true/>
-                            <TextField label="last_name" title="Last Name" place_holder="Mama" required=true/>
+                            <TextField label="first_name" title="Jorge" place_holder="First Name" required=true/>
+                            <TextField label="last_name" title="Last Name" place_holder="Lewis" required=true/>
                             <TextField label="company_name" title="Company Name" place_holder="Futino" required=true/>
-                            <BigTextField label="body" title="What would you like to say? :)" place_holder="I'd like to ask..." required=true/>
+                            //
                         </div>
 
+                        <BigTextField label="body" title="What would you like to say? :)" place_holder="I'd like to ask..." required=true/>
 
                         <div class="mb-6">
                         <TextField label="email" title="Email Address" place_holder="contact@futino.com" required=true/>
@@ -28,6 +29,3 @@ pub fn ContactForm(props: &Props) -> Html {
                     </form>
     }
 }
-
-block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 
-block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500
