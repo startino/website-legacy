1ae813aa36b284a1750de947c12326c7ff07bc63
Jorge Lewis <antopiahk@gmail.com>
2023-01-09 18:01:12 +0800

    footer texts

--- src/components/organisms/footer/mod.rs
+++ src/components/organisms/footer/mod.rs
@@ -9,10 +9,10 @@ use crate::components::*;
 pub fn Footer(props: &Props) -> Html {
     html! {
 
-    <footer class="p-4 rounded-lg shadow border-secondary-300/10">
+    <footer class="p-6 rounded-lg shadow border-secondary-300/10">
         <div class="sm:flex sm:items-center sm:justify-between">
                 <Logo style="labeled" />
-            <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+            <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400 space-x-6">
                 <li>
                     <TextLink text="About" href="/about" />
                 </li>
@@ -27,8 +27,8 @@ pub fn Footer(props: &Props) -> Html {
                 </li>
             </ul>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto border-secondary-300/10 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">{"©"}<a href="#" class="hover:underline">{"Futino."}</a>{" All Rights Reserved."}
+        <hr class="my-6 sm:mx-auto border-secondary-300/10 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">{"©"}<a href="#" class="text-primary-400 hover:underline hover:text-accent-400">{"Futino."}</a>{" All Rights Reserved."}
         </span>
     </footer>
         }
