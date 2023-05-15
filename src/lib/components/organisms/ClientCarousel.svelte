795c07367beebe64d02e4177348f7f6ec840a068
Jorge Lewis <antopiahk@gmail.com>
2023-05-15 16:09:01 +0800

    Update ClientCarousel.svelte

--- src/lib/components/organisms/ClientCarousel.svelte
+++ src/lib/components/organisms/ClientCarousel.svelte
@@ -1,5 +1,24 @@
-<script lang="ts"></script>
+<script lang="ts">
+	let scroll: any;
+	let carouselSpeed = 1;
+</script>
 
+<svelte:window bind:scrollY={scroll} />
 <div class="w-screen">
-	<div class="" />
+	<div class="inline-block">
+		<div class="box bg-blue-500" />
+		<div class="box bg-green-500" />
+		<div class="box bg-yellow-500" />
+		<div class="box bg-amber-500" />
+		<div class="box bg-orange-500" />
+		<div class="box bg-pink-500" />
+		<div class="box bg-red-500" />
+		<div class="box bg-white" />
+	</div>
 </div>
+
+<style>
+	.box {
+		@apply h-24 w-48 rounded-lg m-3;
+	}
+</style>
