a4166dabf874747c76c6125d61b112412efdb5a4
Jorge Lewis <antopiahk@gmail.com>
2023-05-15 14:51:52 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import Header from '$lib/components/organisms/Header.svelte';
 	import Footer from '$lib/components/organisms/Footer.svelte';
 	import Button from '$lib/components/atoms/Button.svelte';
+	import ChapterMenu from '$lib/components/organisms/ChapterMenu.svelte';
 </script>
 
 <Header />
@@ -10,6 +11,7 @@
 <main
 	class="py-40 px-4 border-b shadow-2xl sm:px-6 md:px-8 border-primary-light/40 dark:border-primary-dark/40"
 >
+	<ChapterMenu />
 	<div class="grid justify-items-center space-y-12 mx-auto">
 		<h1 class="display-large">Futino</h1>
 
