26554c8e0c76a33dfbfbfd5f48eeb0e6da9f404e
Jonas Lindberg <eksno@protonmail.com>
2023-06-02 15:11:26 +0200

    implement csp and remove gtag

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import cspDirectives from './csp-directives.mjs';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -18,9 +19,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-
-		paths: {
-			base: ''
+		csp: {
+			mode: 'auto',
+			directives: cspDirectives
 		}
 	}
 };
